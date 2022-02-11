e34dc8b66102f0d80ac6e57ae2424816b9d671f8
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-11 00:28:27 -0500

    added model/ category

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,18 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    category_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+      category_name: {
+        type: DataTypes.STRING 
+    } 
   },
   {
     sequelize,
