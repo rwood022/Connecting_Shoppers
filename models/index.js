ea7878d1060cd80bcfbd49cf66078f2fadc977a7
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-12 21:17:20 -0500

    added association methods

--- models/index.js
+++ models/index.js
@@ -6,11 +6,35 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category-id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products_tag'
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_products'
+});
 
 module.exports = {
   Product,
